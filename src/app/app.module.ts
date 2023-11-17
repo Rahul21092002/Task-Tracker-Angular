162c873cbfe91f5cae7e887bbe605785c9c68186
R Rahul <rahulramu21092002@gmail.com>
2023-11-17 14:27:33 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule,Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+// Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutComponent } from './components/about/about.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+const appRoutes: Routes = [
+  {path : '',component : TasksComponent},
+  {path : 'about',component: AboutComponent}
+]
 
 @NgModule({
   declarations: [
@@ -15,12 +29,18 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    AboutComponent,
+    AddTaskComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes,{enableTracing: true}),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input,Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/Task'; 
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-task-item',
@@ -9,12 +10,23 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 })
 export class TaskItemComponent implements OnInit {
 
-  @Input() task: Task | undefined
+  @Input() task: any
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter()
+
   faTimes = faTimes
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(task: Task){
+    this.onDeleteTask.emit(task)
+  }
+
+  onToggle(task: Task){
+    this.onToggleReminder.emit(task)
   }
 
 }
